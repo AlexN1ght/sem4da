29fd81ef39ae0a950e10da98c91a2f6b1c88185d
Alex Night <alexiscom@icloud.com>
2020-03-15 00:05:17 +0300

    done lr5

--- 5lr(3)/gen.py
+++ 5lr(3)/gen.py
@@ -1,7 +1,7 @@
 import random
 
-for i in range (100000):
+for i in range (80000):
     print(chr(random.randint(97,122)), end='')
 print()
-for i in range (1000000):
+for i in range (10):
     print(chr(random.randint(97,122)), end='')
